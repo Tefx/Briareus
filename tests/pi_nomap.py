f254f3bce8832a9a049130f023fc8326e4674938
Teƒx <zhaomeng.zhu@gmail.com>
2012-09-15 02:31:04 +0800

    new pi demo

---
+++ tests/pi_nomap.py
@@ -0,0 +1,25 @@
+import random
+total_tests = 5000000
+
+def monteCarlo(num_test):
+  """
+  Throw num_test darts at a square
+  Return how many appear within the quarter circle
+  """
+  num_in_circle = 0
+  for _ in xrange(num_test):
+    x = random.random()
+    y = random.random()
+    if x*x + y*y < 1.0:  #within the quarter circle
+      num_in_circle += 1
+  return num_in_circle
+
+
+def calcPi():
+  num_in_circle = monteCarlo(total_tests)
+  pi = (4 * num_in_circle) / float(total_tests)
+  return pi
+
+if __name__ == '__main__':
+  pi = calcPi()
+  print 'Pi determined to be %s' % pi
